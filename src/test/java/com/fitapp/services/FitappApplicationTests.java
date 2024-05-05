67200c7bb932b025ddc98a3f63c292e1c0b5934e
neerajrajak <neeraj92.rajak@gmail.com>
2024-05-05 22:46:59 +0530

    initial commit

---
+++ src/test/java/com/fitapp/services/FitappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitapp.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
